e90e1f1b2bd5ecd53ad547077151666794f39387
Adnan Saleem <adnan13893@gmail.com>
2022-02-24 22:41:12 +0500

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
-import {Box} from '@mui/material'
+import {Box, Container, Grid} from '@mui/material'
 import Heading from './components/Heading'
 import Stopwatch from './components/Stopwatch'
 import Buttons from './components/Buttons'
@@ -11,25 +11,34 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          flexWrap: 'wrap',
-          alignContent: 'space-around',
-          p: 2,
-          m: 0,
-          bgcolor: 'background.paper',
-          borderRadius: 1
-        }}
-        className="app"
-      >
-        <Heading />
-        <Stopwatch />
-        <Buttons />
-        <Laps />
-      </Box>
+      <Container maxWidth="md">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={6} lg={8}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
+                flexWrap: 'wrap',
+                alignContent: 'space-around',
+                p: 2,
+                m: 1,
+                bgcolor: 'background.paper',
+                borderRadius: 2
+              }}
+              className="app"
+            >
+              <Heading />
+              <Stopwatch />
+              <Buttons />
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6} lg={4}>
+            <Laps />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
